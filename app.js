84444724dd69d2caf0f6ee1616a44a092db31669
Sofia Veitko <bon-sofiya@yandex.ru>
2023-06-27 23:35:42 +0300

    fix: внести правки по ревью ПР13

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const router = require('./routes');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
@@ -19,11 +27,12 @@ app.use((req, res, next) => {
 app.use(router);
 app.use((req, res, next) => {
   res.status(404);
-  if (req.accepts('json')) {
-    res.json({ message: 'Not found' });
-    next();
-  }
+  res.json({ message: 'Not found' });
+  next();
 });
+app.use(limiter);
+app.use(helmet());
+app.disable('x-powered-by');
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
