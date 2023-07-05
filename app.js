db0697d9ef3d6640a4956658549d3d7b988a38df
Sofia Veitko <bon-sofiya@yandex.ru>
2023-07-06 01:39:21 +0300

    fix: добавить исключение линтера

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use((err, req, res, next) => {
     next(err);
   }
 });
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
